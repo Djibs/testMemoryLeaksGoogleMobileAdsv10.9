4aebc4d31b3ef228b5c860ce24fc919611d7376b
djibs <iphoneaddict.fr@gmail.com>
2023-08-07 09:38:14 +0200

    Initial Commit

---
+++ testMemoryLeaksGoogleMobileAds/testMemoryLeaksGoogleMobileAdsApp.swift
@@ -0,0 +1,17 @@
+//
+//  testMemoryLeaksGoogleMobileAdsApp.swift
+//  testMemoryLeaksGoogleMobileAds
+//
+//  Created by Cédric CALISTI on 07/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct testMemoryLeaksGoogleMobileAdsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
